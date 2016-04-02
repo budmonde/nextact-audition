f86a24482a4642346540fc57dfefb34f9aa21234
Budmonde Duinkharjav <budmonde@gmail.com>
2016-04-01 20:00:07 -0400

    bug fix

---
+++ client/templates/signup/admin.js
@@ -0,0 +1,34 @@
+var days = ['Monday', 'Tuesday', 'Wednesday'];
+var slots = ['7:00PM - 7:15PM', '7:15PM - 7:30PM', '7:30PM - 7:45PM', '7:45PM - 8:00PM','8:00PM - 8:15PM', '8:15PM - 8:30PM', '8:30PM - 8:45PM', '8:45PM - 9:00PM','9:00PM - 9:15PM', '9:15PM - 9:30PM', '9:30PM - 9:45PM', '9:45PM - 10:00PM'];
+
+Template.admin.helpers({
+	slots: slots
+});
+
+Template.adminslot.helpers({
+  days: days,
+  row: function () {
+    var index = slots.indexOf(this.toString());
+    return index;
+  }
+});
+
+Template.adminday.helpers({
+  row: function (parent) {
+    var index = slots.indexOf(parent.toString());
+    return index;
+  },
+  col: function () {
+    var index = days.indexOf(this.toString());
+    return index;
+  },
+  email: function (parent) {
+    var row = slots.indexOf(parent.toString());
+    var col = days.indexOf(this.toString());
+    var owner = Signups.findOne({slot: [col, row]});
+    if (owner.userId) {
+      return Meteor.users.findOne({_id: owner.userId}).emails[0].address;
+    };
+    return undefined;
+  },
+});
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,14 @@
 Meteor.publish('signups', function() {
 	return Signups.find();
 });
+
+Meteor.publish("allUsers", function(){
+	var user = Meteor.users.findOne({
+        _id: this.userId
+    });
+    return Meteor.users.find({}, {
+        fields: {
+            emails: 1,
+        }
+    });
+});
